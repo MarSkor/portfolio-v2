8e3b515b4a2005af65ee143c4ffe01fd09455091
Martine <maskor_@outlook.com>
2026-02-08 16:36:06 +0100

    added token to sanity lib and fixed queries

--- src/sanity/lib/queries.js
+++ src/sanity/lib/queries.js
@@ -2,11 +2,11 @@ import { defineQuery } from "next-sanity";
 
 export const FEATURED_PROJECTS =
   defineQuery(`*[_type == "project" && featured == true && defined(slug.current)]|order(_createdAt desc){
-  _id,_createdAt, title, techStack, coverImage, slug
+  _id,_createdAt, title, techStack, coverImage, slug, "categoryName": category->title
 }`);
 
 export const ALL_PROJECTS = defineQuery(`
-  *[_type == "project" && ($category == "all" || category->slug.current == $category)] | order(_createdAt desc) {
+  *[_type == "project" && ($category == "all" || category->slug.current == $category)] | order(_createdAt asc) {
     _id, 
     _createdAt, 
     title, 
@@ -20,7 +20,32 @@ export const ALL_PROJECTS = defineQuery(`
 `);
 
 export const PROJECT_DATA_BY_SLUG = defineQuery(`
-  *[_type == "project" && slug.current == $slug][0]
+    *[_type == "project" && slug.current == $slug][0]{
+    _id,
+    slug,
+    title,
+    description,
+    _createdAt,
+    body[]{
+        ...,
+        (_type == "image") => {
+          ...,
+          asset->
+        }
+      },
+    "categoryName": category->title,
+    collaboration,
+    coverImage,
+    date,
+    excerpt,
+    featured,
+    githubUrl,
+    liveUrl,
+    "platformType": platforms,
+    projectGallery,
+    role,
+    techStack
+  }
 `);
 
 export const CATEGORIES_DATA = defineQuery(`
