1659086e91f74d9cccb196793c36c36207890276
Martine <maskor_@outlook.com>
2026-02-08 16:41:50 +0100

    sanity fix, formspree

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   /* config options here */
   reactCompiler: true,
   images: {
+    deviceSizes: [320, 440, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     remotePatterns: [
       {
         protocol: "https",
